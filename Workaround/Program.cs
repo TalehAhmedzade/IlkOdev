e23e4bab26625be82678e3a860a6fc89a34b5903
Taleh <taleh.ahmed2004@gmail.com>
2024-02-08 16:41:39 +0400

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+string mesaj = "MErabalar";
+double tutar = 1000.1;
+int sayi = 12;
+bool mantik=false;
+
+string Name = "Taleh";
+string Surname = "Ahmedzade";
+int yearOfBirth = 2004;
+long FIN = 1111112;
